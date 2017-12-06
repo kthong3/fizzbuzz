36cfe1b8fd68b2e5a4ca8e92e7c51d0c7d480cb6
kthong3 <kthongsakounh@gmail.com>
2017-12-06 12:52:38 -0600

    start psuedocoding and spec file

---
+++ fizzbuzz.rb
@@ -0,0 +1,7 @@
+# input: integer
+# output: array of strings
+
+# create empty array
+# loop: count from 1 to integer
+# if integer is divisible by 3, add "fizzbuzz" instead
+#
---
+++ fizzbuzz_spec.rb
@@ -0,0 +1,5 @@
+require_relative 'fizzbuzz'
+
+describe "fizzbuzz" do
+
+end
